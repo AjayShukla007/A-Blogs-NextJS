a45ac27e137adcb01d7fae459c76f019f1622258
AjayShukla007 <shuklaajay0838@gmail.com>
2024-05-12 11:17:26 +0530

    Added temp navbar in main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="flex gap-2 m-1">
+          <h1 className="text-cyan-200">myNav</h1>
+          <Link href={"/"} >Home</Link>
+          <Link href={"/about"} >About</Link>
+        </nav>
+        {children}</body>
     </html>
   );
 }
